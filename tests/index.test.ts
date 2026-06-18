b960ac83cd4238ce8861d78409d94efe769a33fb
Cezar Augusto <boss@cezaraugusto.net>
2026-06-17 23:46:18 -0300

    Apply eslint-config-auditor formatting and lint rules

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -3,7 +3,7 @@ import {expect, test, describe} from 'vitest'
 import operaLocation from '../src/index'
 
 describe('opera-location2 module', () => {
-  test('returns string or null', () => {
+  it('returns string or null', () => {
     const res = operaLocation()
 
     expect(typeof res === 'string' || res === null).toBe(true)
